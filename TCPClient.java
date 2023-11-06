acd90ce0fac2e260d7a6b2bac5323a422675ad00
Dieynaba Ba <dieynaba.ba6675@gmail.com>
2023-11-05 19:15:06 -0600

    stuff to do left is 3, 5, and 6 for server

--- TCPClient.java
+++ TCPClient.java
@@ -1,33 +1,30 @@
-import java.io.*; 
-import java.net.*; 
-class TCPClient { 
+import java.io.*;
+import java.net.*;
 
-    public static void main(String argv[]) throws Exception 
-    { 
-        String sentence; 
-        String modifiedSentence; 
+class TCPClient {
 
-        BufferedReader inFromUser = 
-          new BufferedReader(new InputStreamReader(System.in)); 
+  public static void main(String argv[]) throws Exception {
+    String sentence;
+    String modifiedSentence;
 
-        Socket clientSocket = new Socket("127.0.0.1", 6789); 
+    BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
 
-        DataOutputStream outToServer = 
-          new DataOutputStream(clientSocket.getOutputStream()); 
-        
-        BufferedReader inFromServer = 
-                new BufferedReader(new
-                InputStreamReader(clientSocket.getInputStream())); 
+    Socket clientSocket = new Socket("127.0.0.1", 6789);
 
-              sentence = inFromUser.readLine(); 
+    DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
 
-              outToServer.writeBytes(sentence + '\n'); 
+    BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
-              modifiedSentence = inFromServer.readLine(); 
+    sentence = inFromUser.readLine();
 
-              System.out.println("FROM SERVER: " + modifiedSentence); 
+    //replaced sentence with equation for testing
+    outToServer.writeBytes("5 + 5" + '\n');
 
-              clientSocket.close(); 
-                         
-          } 
-      } 
+    modifiedSentence = inFromServer.readLine();
+
+    System.out.println("FROM SERVER: " + modifiedSentence);
+
+    clientSocket.close();
+
+  }
+}
--- TCPServer.java
+++ TCPServer.java
@@ -1,32 +1,81 @@
-import java.io.*; 
-import java.net.*; 
+import java.io.*;
+import java.net.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Date;
 
-class TCPServer { 
+class TCPServer {
 
-  public static void main(String argv[]) throws Exception 
-    { 
-      String clientSentence; 
-      String capitalizedSentence; 
+  public static void main(String argv[]) throws Exception {
+    String clientMath;
 
-      ServerSocket welcomeSocket = new ServerSocket(6789); 
-  
-      while(true) { 
-  
-            Socket connectionSocket = welcomeSocket.accept(); 
+    String math[];
 
-           BufferedReader inFromClient = 
-              new BufferedReader(new
-              InputStreamReader(connectionSocket.getInputStream())); 
+    ServerSocket welcomeSocket = new ServerSocket(6789);
+    // Array list to store clients
+    List<ClientInfo> connectedClients = new ArrayList<>();
+    while (true) {
+
+      Socket connectionSocket = welcomeSocket.accept();
+
+      BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+
+      ClientInfo client = new ClientInfo(connectionSocket);
+
+      // adding new client to array list
+      connectedClients.add(client);
+
+      System.out.println("Received from addr: " + client.getAddress());
+
+      DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+
+      clientMath = inFromClient.readLine();
+
+      math = clientMath.split(" ");
+
+      int calculation = 0;
+      if (math[1].equals("+")) {
+        calculation = Integer.parseInt(math[0]) + Integer.parseInt(math[2]);
+
+      } else if (math[1].equals("-")) {
+        calculation = Integer.parseInt(math[0]) - Integer.parseInt(math[2]);
+
+      } else if (math[1].equals("*")) {
+        calculation = Integer.parseInt(math[0]) * Integer.parseInt(math[2]);
+
+      } else {
+        calculation = Integer.parseInt(math[0]) / Integer.parseInt(math[2]);
+      }
+
+      System.out.println(calculation);
+      outToClient.writeInt(calculation);
 
 
-           DataOutputStream  outToClient = 
-             new DataOutputStream(connectionSocket.getOutputStream()); 
+    }
+  }
+}
 
-           clientSentence = inFromClient.readLine(); 
+// Class to create new clients as well as keep track of the clients info
+class ClientInfo {
+  private Socket client;
+  private Date connectionDate;
 
-           capitalizedSentence = clientSentence.toUpperCase() + '\n'; 
+  public ClientInfo(Socket client) {
+    this.client = client;
+    this.connectionDate = new Date();
 
-           outToClient.writeBytes(capitalizedSentence); 
-        } 
-    } 
-} 
+  }
+
+  public InetAddress getAddress() {
+    return client.getInetAddress();
+  }
+
+  public Date getDate() {
+    return connectionDate;
+  }
+
+  public Socket getClient() {
+    return client;
+  }
+
+}
